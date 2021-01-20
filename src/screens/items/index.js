505eb4e2ce57c13af702ab815522b3224e550e91
HoTung <tunghtse130344@fpt.edu.vn>
2021-01-20 16:47:51 +0700

    showMap

--- src/screens/items/index.js
+++ src/screens/items/index.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, ScrollView } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+import  MapScreen  from "../map/ggMap";
+import ResultsList from '../../components/molecules/ResultsList';
 import SearchBar from '../../components/atoms/SearchBar';
 import useResults from '../../hooks/useResults';
-import ResultsList from '../../components/molecules/ResultsList';
 
 const SearchScreen = () => {
 
@@ -18,24 +20,7 @@ const SearchScreen = () => {
 
     return (
         <View style = {{flex: 1}}>
-            <SearchBar
-                term={term}
-                onTermChange={newTerm => setTerm(newTerm)}
-                onTermSubmit={() => searchApi(term)}
-            />
-            {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <Text>We have found {results.length}</Text>
-            <ScrollView>
-                <ResultsList 
-                    results={filterResultsByPrice('$')} 
-                    title="Cost Effective" 
-                    
-                />
-                <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricer" />
-                <ResultsList results={filterResultsByPrice('$')} title="Big Spender" />
-            </ScrollView>
-
-
+            <MapScreen/>
         </View>
     );
 
