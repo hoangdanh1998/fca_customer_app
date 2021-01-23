678abee8196cc7d9b09a98c3eba55e68f77c75be
HoTung <tunghtse130344@fpt.edu.vn>
2021-01-23 10:38:44 +0700

    [WIP] findCurrentLocation

--- src/screens/map/ggMap.js
+++ src/screens/map/ggMap.js
@@ -1,22 +1,43 @@
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import React, { useState } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 const MapScreen = () => {
+
+  const [valueMarginBottom, setValueMarginBottom] = useState(1);
+  // const _onMapReady = () => setValueMarginBottom(0)
+
   return (
     <View style={styles.container}>
       <MapView
+        style={{flex: 1, paddingBottom: {valueMarginBottom}}}
+        onMapReady={() => {
+          PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+          ).then(granted => {
+            alert(granted) // just to ensure that permissions were granted
+          });
+        }}
+        provider={PROVIDER_GOOGLE}
+        followsUserLocation={true}
+        showsUserLocation={true}
+        showUserLocationButton={true}
+        showsCompass={true}
+        toolbarEnabled={true}
+        zoomEnabled={true}
+        rotateEnabled={true}
         style={styles.map}
         provider={PROVIDER_GOOGLE}
         initialRegion={{
           latitude: 10.765,
-          longitude: 106.660,
+          longitude: 106.66,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
         showsUserLocation={true}
         showsMyLocationButton={true}
-      />
+        
+      ></MapView>
     </View>
   );
 };
@@ -29,9 +50,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   map: {
-    height: '100%',
-    width: '100%',
-    
+    height: "100%",
+    width: "100%",
   },
 });
 
