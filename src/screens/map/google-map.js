16b848f07df49ef3cb9502a4b37f3dbde3e5e232
Hoang Danh <danh02071998@gmail.com>
2021-01-23 11:36:06 +0700

    Ask permission current location

--- src/screens/map/google-map.js
+++ src/screens/map/google-map.js
@@ -1,42 +1,49 @@
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 const MapScreen = () => {
+  const [initialRegion, setInitialRegion] = useState({
+    latitude: 10.765,
+    longitude: 106.66,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  })
 
-  const [valueMarginBottom, setValueMarginBottom] = useState(1);
-  // const _onMapReady = () => setValueMarginBottom(0)
+
+  const _onMapReady = useCallback(async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== `granted`) {
+      console.log(`Permisson Denied`);
+    }
+    const location = await Location.getCurrentPositionAsync({ "accuracy": Location.Accuracy.High });
+    setInitialRegion({
+      ...initialRegion,
+      "latitude": location.coords.latitude,
+      "longitude": location.coords.longitude
+    });
+  }, [initialRegion]);
+  console.log(initialRegion);
+  // useEffect(() => {
+  //   _onMapReady();
+  // }, [_onMapReady]);
 
   return (
     <View style={styles.container}>
       <MapView
-        style={{ flex: 1, paddingBottom: { valueMarginBottom } }}
-        onMapReady={() => {
-          PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-          ).then(granted => {
-            alert(granted) // just to ensure that permissions were granted
-          });
-        }}
-        provider={PROVIDER_GOOGLE}
         followsUserLocation={true}
-        showsUserLocation={true}
-        showUserLocationButton={true}
-        showsCompass={true}
-        toolbarEnabled={true}
-        zoomEnabled={true}
-        rotateEnabled={true}
+        onMapReady={_onMapReady}
+        showsUserLocation
+        showsScale
+        showsCompass
+        toolbarEnabled
+        zoomEnabled
+        rotateEnabled
         style={styles.map}
         provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: 10.765,
-          longitude: 106.66,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        showsUserLocation={true}
-        showsMyLocationButton={true}
-
+        initialRegion={initialRegion}
       ></MapView>
     </View>
   );
