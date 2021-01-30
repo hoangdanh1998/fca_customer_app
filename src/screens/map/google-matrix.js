4834f47940c7e2cf7c4d680454b3922b5cde02f4
HoTung <tunghtse130344@fpt.edu.vn>
2021-01-30 15:04:52 +0700

    them matrix tinh distance + duration va update linh tinh

--- src/screens/map/google-matrix.js
+++ src/screens/map/google-matrix.js
@@ -3,33 +3,59 @@ import React, { useEffect, useState } from "react";
 
 import { KEY_GGMAP } from "../../constance/constance";
 
-export default () => {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  const ggAPIUrl =
-    "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial";
+const GoogleMatrix = () => {
+  // const dumData=[{
+  //   lat_destination: 10.8414899,
+  //   lng_destination: 106.8078577,
+  // }]
+  // const [isLoading, setLoading] = useState(true);
+  // const [data, setData] = useState([]);
+    const [matrix,setMatrix]=useState(null)
+  const google_APIUrl =
+    `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=10.8277609,106.6793874&destinations=10.8224989,106.6880843&key=${KEY_GGMAP}`;
   // "&origin="
   // "&destination="
   // "&key="${KEY_GGMAP}
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: "san jose",
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (error) {
-      seterrorMessage("Something went wrong");
-    }
-  };
-
+  
   useEffect(() => {
-    searchApi("pasta");
+    (async () => {
+      fetch(google_APIUrl)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson.rows[0].elements[0].distance.text)
+        console.log(responseJson.rows[0].elements[0].duration.text)
+        // setMatrix({
+        //   distance:responseJson.distance,
+        //   duration:responseJson.duration
+        // })
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    })();
   }, []);
 
-  return [errorMessage, results, searchApi];
-};
+
+  // matrixCallApi = async () => {
+  //   fetch(google_APIUrl)
+  //     .then((response) => response.json())
+  //     .then((responseJson) => {
+  //       console.log(responseJson.rows.elements.distance.text)
+  //       console.log(responseJson.rows.elements.duration.text)
+  //       setMatrix({
+  //         distance:responseJson.rows.elements.distance.text,
+  //         duration:responseJson.rows.elements.duration.text
+  //       })
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }
+  return(
+    <View>
+   
+    </View>
+  );
+}
+
+  export default GoogleMatrix;
