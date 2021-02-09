a38ec533f80a21361c1b90dfe2cb82fd3f2af0dc
pkimanh03 <pkimanh03@gmail.com>
2021-02-09 13:37:08 +0700

    fix constant syntax

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,42 +4,42 @@ export const KEY_GOOGLE_MAP = "AIzaSyCbsRxuA3NGxpOMeC0UkmJTi0V2bvSyAJo";
 // APP CONFIG
 export const DATE_FORMAT = "DD/MM/YYYY";
 export const LANGUAGE = {
-    VI: 'vi',
-    EN: 'en',
-}
+  VI: "vi",
+  EN: "en",
+};
 
 // APP STATUS
 export const OrderStatus = {
-    INITIALIZATION = 'INITIALIZATION',
-    ACCEPTANCE = 'ACCEPTANCE',
-    REJECTION = 'REJECTION',
-    CANCELLATION = 'CANCELLATION',
-    CLOSURE = 'CLOSURE',
-    PREPARATION = 'PREPARATION',
-    READINESS = 'READINESS',
-    ARRIVAL = 'ARRIVAL',
-    RECEPTION = 'RECEPTION',
-    WAIT = 'WAIT',
-}
+  INITIALIZATION: "INITIALIZATION",
+  ACCEPTANCE: "ACCEPTANCE",
+  REJECTION: "REJECTION",
+  CANCELLATION: "CANCELLATION",
+  CLOSURE: "CLOSURE",
+  PREPARATION: "PREPARATION",
+  READINESS: "READINESS",
+  ARRIVAL: "ARRIVAL",
+  RECEPTION: "RECEPTION",
+  WAIT: "WAIT",
+};
 
 export const PartnerStatus = {
-    PROCESS = 'PROCESS',
-    REJECTED = 'REJECTED',
-    APPROVED = 'APPROVED',
-}
+  PROCESS: "PROCESS",
+  REJECTED: "REJECTED",
+  APPROVED: "APPROVED",
+};
 
 export const CustomerStatus = {
-    ACTIVE = 'ACTIVE',
-    PASSIVE = 'PASSIVE',
-}
+  ACTIVE: "ACTIVE",
+  PASSIVE: "PASSIVE",
+};
 
 export const FCAItemStatus = {
-    ACTIVE = 'ACTIVE',
-    ARCHIVE = 'ARCHIVE',
-}
+  ACTIVE: "ACTIVE",
+  ARCHIVE: "ARCHIVE",
+};
 
 export const RequestItemStatus = {
-    PROCESS = 'PROCESS',
-    APPROVED = 'APPROVED',
-    REJECTED = 'REJECTED',
-}
+  PROCESS: "PROCESS",
+  APPROVED: "APPROVED",
+  REJECTED: "REJECTED",
+};
