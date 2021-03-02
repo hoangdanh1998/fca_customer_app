e9ee8a94d03f4cee5ac04182b97180ae706ec7db
Hoang Danh <danh02071998@gmail.com>
2021-03-03 00:18:19 +0700

    add ref order

--- src/service/firebase/firebase-realtime.js
+++ src/service/firebase/firebase-realtime.js
@@ -1,6 +1,6 @@
 import * as firebase from 'firebase';
 export const setTrackingOrder = (orderId, timeRemain) => {
-    firebase.database().ref(orderId)
+    firebase.database().ref('order').child(orderId)
         .update({
             id: orderId,
             timeRemain,
