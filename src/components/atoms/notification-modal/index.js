e8fa1e1a5fb3a348980c5f08e10dfce4b2cf0d9a
pkimanh03 <pkimanh03@gmail.com>
2021-03-10 15:35:02 +0700

    alert modal

--- src/components/atoms/notification-modal/index.js
+++ src/components/atoms/notification-modal/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import AwesomeAlert from "react-native-awesome-alerts";
 import { Modal, Text } from "react-native";
 import { Card, CardItem, Icon } from "native-base";
 
@@ -11,29 +12,48 @@ const NotificationModal = (props) => {
   const message = props.message;
   const visible = props.visible;
   return (
-    <Modal animationType="slide" transparent visible={visible}>
-      <Card style={styles.card}>
-        <CardItem style={{ flex: 1 }}>
-          <Icon
-            name={
-              message === MESSAGES.FAIL || message === MESSAGES.REJECTED
-                ? "close-circle-outline"
-                : "checkmark-circle-outline"
-            }
-            style={
-              message === MESSAGES.FAIL || message === MESSAGES.REJECTED
-                ? styles.fail
-                : styles.success
-            }
-          />
-        </CardItem>
-        <CardItem style={{ flex: 1 }}>
-          <Text style={styles.message}>
-            {IMLocalized(`wording-${message}`)}
-          </Text>
-        </CardItem>
-      </Card>
-    </Modal>
+    // <Modal animationType="slide" transparent visible={visible}>
+    //   <Card style={styles.card}>
+    //     <CardItem style={{ flex: 1 }}>
+    //       <Icon
+    //         name={
+    //           message === MESSAGES.FAIL || message === MESSAGES.REJECTED
+    //             ? "close-circle-outline"
+    //             : "checkmark-circle-outline"
+    //         }
+    //         style={
+    //           message === MESSAGES.FAIL || message === MESSAGES.REJECTED
+    //             ? styles.fail
+    //             : styles.success
+    //         }
+    //       />
+    //     </CardItem>
+    //     <CardItem style={{ flex: 1 }}>
+    //       <Text style={styles.message}>
+    //         {IMLocalized(`wording-${message}`)}
+    //       </Text>
+    //     </CardItem>
+    //   </Card>
+    // </Modal>
+    <AwesomeAlert
+      show={visible}
+      showProgress={false}
+      title="AwesomeAlert"
+      message={IMLocalized(`wording-${message}`)}
+      closeOnTouchOutside={true}
+      closeOnHardwareBackPress={false}
+      // showCancelButton={true}
+      // showConfirmButton={true}
+      // cancelText="No, cancel"
+      // confirmText="Yes, delete it"
+      // confirmButtonColor="#DD6B55"
+      // onCancelPressed={() => {
+      //   this.hideAlert();
+      // }}
+      // onConfirmPressed={() => {
+      //   this.hideAlert();
+      // }}
+    />
   );
 };
 
